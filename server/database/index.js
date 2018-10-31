0e593abf337e485dbee1116e894e81de038d2d8b
Ravuthz <vannaravuth.yo@sisgroup.sg>
2018-10-31 14:31:30 +0700

    Connect to mysql with Sequelize

---
+++ server/database/index.js
@@ -0,0 +1,17 @@
+const Sequelize = require("sequelize");
+
+const sequelize = new Sequelize("koa_db", "ravuthz", "123123", {
+  host: "localhost",
+  dialect: "mysql",
+
+  pool: {
+    max: 5,
+    min: 0,
+    acquire: 30000,
+    idle: 10000
+  },
+
+  operatorsAliases: false
+});
+
+module.exports = sequelize;
